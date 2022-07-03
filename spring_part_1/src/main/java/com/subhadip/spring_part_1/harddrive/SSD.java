17e98d5158d33dcd2fa391f8fefdc5dbd99b8a58
subhadip1203 <subhadip1203@gmail.com>
2022-07-03 12:09:52 +0530

    Component and bin

---
+++ spring_part_1/src/main/java/com/subhadip/spring_part_1/harddrive/SSD.java
@@ -0,0 +1,46 @@
+package com.subhadip.spring_part_1.harddrive;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SSD {
+  private int storage;
+  private String company;
+
+  public SSD() {
+    super();
+    System.out.println("constrructor running ...");
+  }
+
+  public SSD(int storage, String company) {
+    this.storage = storage;
+    this.company = company;
+  }
+
+
+  public int getStorage() {
+    return this.storage;
+  }
+
+  public void setStorage(int storage) {
+    this.storage = storage;
+  }
+
+  public String getCompany() {
+    return this.company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+      " storage='" + getStorage() + "'" +
+      ", company='" + getCompany() + "'" +
+      "}";
+  }
+
+
+}
