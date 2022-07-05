29334f15ff5946ca1ae8cddc490e0e98da5c5656
subhadip1203 <subhadip1203@gmail.com>
2022-07-05 22:49:00 +0530

    autowire basic

--- spring_part_1/src/main/java/com/subhadip/spring_part_1/SpringPart1Application.java
+++ spring_part_1/src/main/java/com/subhadip/spring_part_1/SpringPart1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.subhadip.spring_part_1.harddrive.SSD;
+import com.subhadip.spring_part_1.laptop.AcerLaptop;
 
 @SpringBootApplication
 public class SpringPart1Application {
@@ -14,11 +14,9 @@ public class SpringPart1Application {
 		System.out.println("server running ...");
 		ConfigurableApplicationContext context = SpringApplication.run(SpringPart1Application.class, args);
 		
-		SSD toshiba = context.getBean(SSD.class);
-		System.out.println(toshiba.toString());
-
-		SSD sony = context.getBean(SSD.class);
-		System.out.println(sony.toString());
+		AcerLaptop toshiba = context.getBean(AcerLaptop.class);
+		toshiba.printSSD();
+		
 
 
 		System.out.println("server running ...");
