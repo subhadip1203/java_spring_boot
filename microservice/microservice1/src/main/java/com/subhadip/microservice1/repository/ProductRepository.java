17397844e8c37ed74a39084d91e9ffab04b812c5
subhadip1203 <subhadip1203@gmail.com>
2023-04-13 16:34:14 -0700

    spring boot microservice 2

--- microservice/microservice1/src/main/java/com/subhadip/microservice1/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.subhadip.microservice1.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.subhadip.microservice1.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    
-}
---
+++ microservice/product-service/src/main/java/com/subhadip/productservice/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.subhadip.productservice.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.subhadip.productservice.dto.ProductRequestDTO;
+import com.subhadip.productservice.dto.ProductResponseDTO;
+import com.subhadip.productservice.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequestDTO productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponseDTO> getAllProducts(){
+        return productService.getAllProduct();
+    }
+}
