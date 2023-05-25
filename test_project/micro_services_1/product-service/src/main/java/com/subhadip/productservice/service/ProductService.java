c2e862b17a24502ee11305ad710b2c6a8db8092b
subhadip1203 <subhadip1203@gmail.com>
2023-05-25 10:58:47 -0700

    spring boot DTO

---
+++ test_project/micro_services_1/product-service/src/main/java/com/subhadip/productservice/service/ProductService.java
@@ -0,0 +1,51 @@
+package com.subhadip.productservice.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.subhadip.productservice.dto.ProductRequestDTO;
+import com.subhadip.productservice.dto.ProductResponseDTO;
+import com.subhadip.productservice.model.Product;
+import com.subhadip.productservice.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    
+    private ProductResponseDTO mapToProductResponseDTO(Product dbProduct) {
+        ProductResponseDTO productResponse = ProductResponseDTO.builder()
+                .id(dbProduct.getId())
+                .name(dbProduct.getName())
+                .description(dbProduct.getDescription())
+                .price(dbProduct.getPrice())
+                .build();
+        return productResponse;
+    }
+    
+    public ProductResponseDTO createProduct(ProductRequestDTO productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        return mapToProductResponseDTO(product);
+
+    }
+
+
+    public List<ProductResponseDTO> getAllProduct() {
+        List<Product> dbProducts = productRepository.findAll();
+
+        return dbProducts.stream().map(product -> mapToProductResponseDTO(product)).toList();
+        // this is the use of Lambda method
+        // both same 
+        // return dbProducts.stream().map(this::mapToProductResponseDTO).toList();
+    }
+
+}
