af766178432caaa75b1fbe1ddd8c0ff649db7e7b
subhadip1203 <subhadip1203@gmail.com>
2023-05-25 09:23:30 -0700

    sring boot

--- microservice/product-service/src/main/java/com/subhadip/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.subhadip.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.subhadip.productservice.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    
-}
